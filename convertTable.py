ef4fa08311c81feef044afc3e512eebae4134bfa
Michael Dowlin <mdowlinus@yahoo.com>
2020-01-04 12:46:04 -0600

    switching cities.csv to the file created in my ealier api project

--- convertTable.py
+++ convertTable.py
@@ -8,9 +8,9 @@ file = os.path.join("Resources","cities.csv")
 file_df = pd.read_csv(file, encoding="ISO-8859-1")
 
 # reformat the date column
-file_df['Date'] = pd.to_datetime(file_df['Date'])
-file_df['Date'] = file_df['Date'].dt.date
+file_df['date'] = pd.to_datetime(file_df['date'])
+file_df['date'] = file_df['date'].dt.date
 
-file_df.set_index('City_ID', inplace=True)
+file_df.set_index('city_id', inplace=True)
 
 file_df.to_html('table.html', index=False)
