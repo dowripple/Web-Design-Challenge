21eede1826c7b35a2a3a449b50ba104e185967d7
Michael Dowlin <mdowlinus@yahoo.com>
2020-01-04 12:47:03 -0600

    fixing py code

--- convertTable.py
+++ convertTable.py
@@ -8,8 +8,8 @@ file = os.path.join("Resources","cities.csv")
 file_df = pd.read_csv(file, encoding="ISO-8859-1")
 
 # reformat the date column
-file_df['date'] = pd.to_datetime(file_df['date'])
-file_df['date'] = file_df['date'].dt.date
+# file_df['date'] = pd.to_datetime(file_df['date'])
+# file_df['date'] = file_df['date'].dt.date
 
 file_df.set_index('city_id', inplace=True)
 
