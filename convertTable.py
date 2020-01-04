17bb06b266f4a3579cb6b29589c8eee0b3d238e0
Michael Dowlin <mdowlinus@yahoo.com>
2020-01-04 11:40:42 -0600

    jquery!

--- convertTable.py
+++ convertTable.py
@@ -1,10 +1,16 @@
 import pandas as pd
 import os
+import datetime as dt
+
 
 file = os.path.join("Resources","cities.csv")
 
 file_df = pd.read_csv(file, encoding="ISO-8859-1")
 
+# reformat the date column
+file_df['Date'] = pd.to_datetime(file_df['Date'])
+file_df['Date'] = file_df['Date'].dt.date
+
 file_df.set_index('City_ID', inplace=True)
 
-file_df.to_html('table.html')
+file_df.to_html('table.html', index=False)
--- table.js
+++ table.js
@@ -1,12 +1,16 @@
 // JavaScript Document
 $(document).ready(function() {
     
-    var $tableDiv = $('#tableDiv')
-
-    alert('Hello!')
-
-    //using .get to get the table html from the file created by python
+   //use ajax .get to retrieve the table html and return to the table div
     $.get("table.html", function(data, status){
-        $tableDiv.html(data);
+        //assign the table html to div
+        $('#tableDiv').html(data);
+
+        //add the bootstrap classes to the table item
+        $('table').each(function(i){
+            $(this).addClass('table',i);
+            $(this).addClass('table-striped',i);
+            $(this).addClass('table-responsive',i);
+        }); 
     });
 });
